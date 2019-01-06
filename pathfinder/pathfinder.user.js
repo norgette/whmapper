190fd391f1d7e9960e55a6b8b1caffdb3a43f060
norgette <46420974+norgette@users.noreply.github.com>
2019-01-07 00:34:32 +1000

    Create pathfinder.user.js

---
+++ pathfinder/pathfinder.user.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name         Best Wormhole
+// @namespace    https://mapper.eveuniversity.org/
+// @version      0.1
+// @description  Finds the best wormhole for a given source or destination.
+// @author       Norgette Audier
+// @match        https://mapper.eveuniversity.org/map/*
+// @require      https://gitlab.com/norganna-eve/pathfinder/raw/master/js/pathfinder.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  const pf = window.PathFinder;
+
+  const finder = $('<button id="btnPath" class="btn btn-xs btn-primary">PathFinder</button>');
+  const search = $('<div id="pathFinderSearch" style="margin: 0.25em 0;">');
+  const searchBox = $('<input type="text" id="findSystem" class="systemAuto form-control input-sm" style="width: 400px; display: inline-block;" placeholder="System Name"/>');
+  const searchButton = $('<button id="btnSearch" class="btn btn-sm btn-primary">Search</button>')
+  const searchResults = $('<div>');
+  search.append(searchBox).append(searchButton).append(searchResults).hide();
+
+  finder.click(() => search.toggle());
+  searchButton.click(() => showPathFinder(searchBox.val()));
+
+  $('.btn-toolbar').append(finder).after(search);
+
+  function showPathFinder(search) {
+    const s = window.systemsJSON;
+    const results = [];
+    const distance = {};
+    const path = {};
+    for (let i = 0; i < s.length; i++) {
+      const ss = s[i];
+      if (pf.jumps[ss.sysID]) {
+        const found = pf.findPath(ss.sysID, search);
+        if (found) {
+          results.push(ss.Name);
+          distance[ss.Name] = found.length + ss.LevelX - 1;
+          const steps = [];
+          for (let j = 0; j < found.length; j++) {
+            const hop = found[j];
+            steps.push(hop.system + ' (' + hop.security + ')')
+          }
+          path[ss.Name] = steps.join(', ');
+        }
+      }
+    }
+
+    searchResults.text('');
+    results.sort((a,b) => distance[a]-distance[b]);
+    for (let i = 0; i < results.length; i++) {
+      const k = results[i];
+      const d = distance[k];
+      const p = path[k];
+      searchResults.append($('<div>').attr('title', p).text(k + ': ' + d + ' jumps'));
+    }
+  }
+})();
